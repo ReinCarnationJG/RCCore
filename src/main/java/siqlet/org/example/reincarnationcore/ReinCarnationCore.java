6d536609ba76e941dffca59bd2513b9d86f561f9
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-21 23:30:31 +0900

    ヨシ

---
+++ src/main/java/siqlet/org/example/reincarnationcore/ReinCarnationCore.java
@@ -0,0 +1,37 @@
+package siqlet.org.example.reincarnationcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import siqlet.org.example.reincarnationcore.command.TradeCommand;
+
+import java.util.Objects;
+
+public final class ReinCarnationCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // if ( ymlが無い ) {
+            getLogger().info("ymlが存在しない為生成しました");
+        // }
+
+        getLogger().info("Event登録中...");
+
+
+
+        getLogger().info("Event登録完了");
+
+        getLogger().info("Command登録中");
+
+        Objects.requireNonNull(getServer().getPluginCommand("trade")).setExecutor(new TradeCommand());
+
+        getLogger().info("Command登録完了");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
