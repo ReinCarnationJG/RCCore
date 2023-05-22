2be40b67d9385cae640e86388f0498201f722201
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-22 18:39:21 +0900

    色々追加

--- src/main/java/siqlet/org/example/reincarnationcore/ReinCarnationCore.java
+++ src/main/java/siqlet/org/example/reincarnationcore/ReinCarnationCore.java
@@ -1,7 +1,11 @@
 package siqlet.org.example.reincarnationcore;
 
+import io.lumine.mythic.bukkit.utils.random.VariableAmount;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import siqlet.org.example.reincarnationcore.command.SabaminCommand;
 import siqlet.org.example.reincarnationcore.command.TradeCommand;
+import siqlet.org.example.reincarnationcore.listener.FixedInventoryListener;
 import siqlet.org.example.reincarnationcore.register.Register;
 
 import java.util.Objects;
@@ -12,21 +16,20 @@ public final class ReinCarnationCore extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-        // if ( ymlが無い ) {
-            getLogger().info("ymlが存在しない為生成しました");
-        // }
-
         getLogger().info("Event登録中...");
 
         getServer().getPluginManager().registerEvents(new Register(), this);
+        getServer().getPluginManager().registerEvents(new FixedInventoryListener(), this);
 
         getLogger().info("Event登録完了");
-
         getLogger().info("Command登録中");
 
         Objects.requireNonNull(getServer().getPluginCommand("trade")).setExecutor(new TradeCommand());
+        Objects.requireNonNull(getServer().getPluginCommand("sabamin")).setExecutor(new SabaminCommand());
 
         getLogger().info("Command登録完了");
+
+        Bukkit.getScheduler().runTaskTimer(this, FixedInventoryListener::Fixed, 0, 1);
 
     }
 
