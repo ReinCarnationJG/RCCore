3ca1cfc32576960a9808802ddb13bf6a05d2a31c
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-22 18:54:04 +0900

    修正というか追加

--- src/main/java/siqlet/org/example/reincarnationcore/listener/FixedInventoryListener.java
+++ src/main/java/siqlet/org/example/reincarnationcore/listener/FixedInventoryListener.java
@@ -2,6 +2,7 @@ package siqlet.org.example.reincarnationcore.listener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,18 +10,23 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import siqlet.org.example.reincarnationcore.ReinCarnationCore;
 import siqlet.org.example.reincarnationcore.config.SabaminFile;
 
 public class FixedInventoryListener implements Listener {
 
-    public static void Barrier(Player p) {
+    private static void Barrier(Player p) {
 
         if (SabaminFile.isEnabled(p.getUniqueId())) {
 
             ItemStack barrierStack = new ItemStack(Material.BARRIER);
             ItemMeta barrierMeta = barrierStack.getItemMeta();
+            NamespacedKey nbtKey = new NamespacedKey(ReinCarnationCore.inst(), "FixedBarrier");
 
             barrierMeta.setDisplayName("§f");
+            barrierMeta.getPersistentDataContainer().set(nbtKey, PersistentDataType.STRING, "1");
+
             barrierStack.setItemMeta(barrierMeta);
 
             p.getInventory().setItem(7, barrierStack);
