b796c711bb467e74385f6edd5e65ea30344ae903
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-27 03:51:18 +0900

    GUIを開く所まで作成

--- src/main/java/siqlet/org/example/reincarnationcore/command/TradeCommand.java
+++ src/main/java/siqlet/org/example/reincarnationcore/command/TradeCommand.java
@@ -8,7 +8,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
+import siqlet.org.example.reincarnationcore.util.ItemUtil;
 import siqlet.org.example.reincarnationcore.util.PlayerUtil;
 
 import java.util.HashMap;
@@ -18,6 +20,9 @@ import java.util.Objects;
 public class TradeCommand implements CommandExecutor {
 
     private final Map<Player, Player> tradeRequests = new HashMap<>();
+    public static Map<Player, Inventory> requesterGUI = new HashMap<>();;
+    public static Map<Player, Inventory> targetGUI = new HashMap<>();;
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -30,29 +35,47 @@ public class TradeCommand implements CommandExecutor {
 
         */
 
+        String prefix = "§d[§eRCCore§d] §f";
+
         if (!(sender instanceof Player player)) return true;
 
+        if ( args.length == 0 ) {
+            player.sendMessage(prefix + "引数を指定してください");
+            return true;
+        }
+
         String arg = args[0];
-        String prefix = "§d[§eRCCore§d] §f";
 
         if (arg.equalsIgnoreCase("accept")) {
 
-            if ( !tradeRequests.containsKey(player) ) {
+            Player requester = tradeRequests.get(player);
+
+            if ( requester == null ) {
                 player.sendMessage(prefix + "届いている申請がありません。");
                 return true;
             }
 
-            Player target = tradeRequests.remove(player);
-            target.sendMessage(prefix + "tradeが承認されました");
+            player.sendMessage(prefix + "trade画面に移行します");
+            requester.sendMessage(prefix + "tradeが承認されました");
+
+            openTargetGUI(player, requester);
+            openRequesterGUI(requester, player);
+
+            tradeRequests.remove(player);
 
         } else {
 
-            if (!PlayerUtil.checkPlayer(arg)) {
+            if ( !PlayerUtil.checkPlayer(arg) ) {
                 player.sendMessage(prefix + "指定されたプレイヤーは存在しません");
+                return true;
+            }
+            if( PlayerUtil.checkPlayerName(arg).equals(player.getName()) ) {
+                player.sendMessage(prefix + "自分にtrade申請を送る事は出来ません。");
+//                return true;
             }
 
             Player targetPlayer = Bukkit.getServer().getPlayer(arg);
-            String messageText = prefix + "§nトレードの申請が届きました！§7クリックして承認する";
+            String messageText = prefix + "§c§nトレードの申請が届きました！§7§lクリックして承認する";
 
             tradeRequests.put(targetPlayer, player);
 
@@ -62,8 +85,70 @@ public class TradeCommand implements CommandExecutor {
 
             Objects.requireNonNull(targetPlayer).spigot().sendMessage(message);
 
+            player.sendMessage(prefix + "§e" + arg + "§fにtrade申請を送信しました！");
+
             return true;
         }
         return true;
+    }
+
+    public static void openTargetGUI(Player target, Player requester) {
+
+        Inventory gui = Bukkit.createInventory(null, 6 * 9, "Trade GUI");
+
+        for ( int i = 0; i <= 53; i++ ) {
+            if ( i <= 8 || i >= 43 ) {
+                gui.setItem(i, ItemUtil.redGlass());
+            }
+            if ( i == 4 || i == 13 || i == 22 || i == 31 || i == 40 || i == 49 ) {
+                gui.setItem(i, ItemUtil.blackGlass());
+            }
+            if ( i == 2) {
+                gui.setItem(i, ItemUtil.getPlayerHead(target));
+            }
+            if ( i == 6) {
+                gui.setItem(i, ItemUtil.getPlayerHead(requester));
+            }
+            if ( i == 47 || i == 51 ) {
+                gui.setItem(i, ItemUtil.goldIngot());
+            }
+            if ( i == 48 || i == 50 ) {
+                gui.setItem(i, ItemUtil.yellowGlass());
+            }
+        }
+
+        targetGUI.put(target, gui);
+        target.openInventory(gui);
+
+    }
+
+    public static void openRequesterGUI(Player requester, Player target) {
+
+        Inventory gui = Bukkit.createInventory(null, 6 * 9, "Trade GUI");
+
+        for ( int i = 0; i <= 53; i++ ) {
+            if ( i <= 9 || i >= 44 ) {
+                gui.setItem(i, ItemUtil.redGlass());
+            }
+            if ( i == 4 || i == 13 || i == 22 || i == 31 || i == 40 || i == 49 ) {
+                gui.setItem(i, ItemUtil.blackGlass());
+            }
+            if ( i == 2) {
+                gui.setItem(i, ItemUtil.getPlayerHead(requester));
+            }
+            if ( i == 6) {
+                gui.setItem(i, ItemUtil.getPlayerHead(target));
+            }
+            if ( i == 47 || i == 51 ) {
+                gui.setItem(i, ItemUtil.goldIngot());
+            }
+            if ( i == 48 || i == 50 ) {
+                gui.setItem(i, ItemUtil.yellowGlass());
+            }
+        }
+
+        requesterGUI.put(requester, gui);
+        requester.openInventory(gui);
+
     }
 }
