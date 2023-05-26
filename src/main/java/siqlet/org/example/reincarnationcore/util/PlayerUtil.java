04a6e14673c9abafeef49cbffc40884c9b4d0482
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-27 03:50:27 +0900

    PlayerのUtil

--- src/main/java/siqlet/org/example/reincarnationcore/util/PlayerUtil.java
+++ src/main/java/siqlet/org/example/reincarnationcore/util/PlayerUtil.java
@@ -3,13 +3,23 @@ package siqlet.org.example.reincarnationcore.util;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class PlayerUtil {
 
     public static boolean checkPlayer(String name) {
 
-        Player target = Bukkit.getServer().getPlayer(name); // プレイヤーオブジェクトを取得
+        Player target = Bukkit.getServer().getPlayer(name);
 
         return target != null;
+
+    }
+
+    public static String checkPlayerName(String name) {
+
+        Player target = Bukkit.getServer().getPlayer(name);
+
+        return Objects.requireNonNull(target).getName();
 
     }
 
