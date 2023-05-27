da440470b0ad35ddd88c99448a058d4f087e1018
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-27 16:30:19 +0900

    実装done

--- src/main/java/siqlet/org/example/reincarnationcore/command/SabaminCommand.java
+++ src/main/java/siqlet/org/example/reincarnationcore/command/SabaminCommand.java
@@ -1,5 +1,6 @@
 package siqlet.org.example.reincarnationcore.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,9 +10,6 @@ import org.jetbrains.annotations.NotNull;
 import siqlet.org.example.reincarnationcore.ReinCarnationCore;
 import siqlet.org.example.reincarnationcore.config.ConfigFile;
 
-import java.io.File;
-
-
 public class SabaminCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -19,21 +17,17 @@ public class SabaminCommand implements CommandExecutor {
         if ( !(sender instanceof Player player) ) return true;
 
         String prefix = "§d[§eRCCore§d] §f";
-        File sabamin = new File("plugins/ReinCarnationCore", "sabamin.yml");
-        YamlConfiguration sabaminYml = YamlConfiguration.loadConfiguration(sabamin);
+        YamlConfiguration sabaminYml = ConfigFile.load(ReinCarnationCore.inst(), "sabamin.yml");
 
-        ConfigFile.load(ReinCarnationCore.inst(), "sabamin.yml");
-
-        if ( sabaminYml.getBoolean("players." + player.getUniqueId(), true) ) {
+        if ( !sabaminYml.getBoolean("players." + player.getUniqueId(), true) ) {
             player.sendMessage(prefix + "鯖民モードを有効化");
             sabaminYml.set("players." + player.getUniqueId(), true);
-            ConfigFile.save(ReinCarnationCore.inst(), "sabamin.yml");
+            ConfigFile.save(ReinCarnationCore.inst(), "sabamin.yml", sabaminYml);
         } else {
             player.sendMessage(prefix + "鯖民モードを無効化");
             sabaminYml.set("players." + player.getUniqueId(), false);
-            ConfigFile.save(ReinCarnationCore.inst(), "sabamin.yml");
+            ConfigFile.save(ReinCarnationCore.inst(), "sabamin.yml", sabaminYml);
         }
-
         return true;
     }
 }
