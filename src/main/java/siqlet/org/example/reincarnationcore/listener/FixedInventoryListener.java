6d71441b825d0356f025d1b4a8754c7147c2301d
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-05-27 16:30:25 +0900

    実装done

--- src/main/java/siqlet/org/example/reincarnationcore/listener/FixedInventoryListener.java
+++ src/main/java/siqlet/org/example/reincarnationcore/listener/FixedInventoryListener.java
@@ -13,9 +13,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import siqlet.org.example.reincarnationcore.ReinCarnationCore;
+import siqlet.org.example.reincarnationcore.config.ConfigFile;
 
 import java.io.File;
-import java.util.Objects;
 
 public class FixedInventoryListener implements Listener {
 
@@ -44,43 +44,46 @@ public class FixedInventoryListener implements Listener {
         }
     }
 
-    private static void clearBarrier(Player p) {
-
-        for (ItemStack item : p.getInventory().getContents()) {
-            if (item != null && item.getType() == Material.BARRIER) {
-
-                ItemMeta itemMeta = item.getItemMeta();
-                NamespacedKey nbtKey = new NamespacedKey(ReinCarnationCore.inst(), "FixedBarrier");
-                String nbtValue = itemMeta.getPersistentDataContainer().get(nbtKey, PersistentDataType.STRING);
-                Inventory inv = p.getInventory();
-
-                if (nbtValue != null && nbtValue.equals("1")) {
-
-                    if (!Objects.requireNonNull(inv.getItem(5)).equals(item) || !Objects.requireNonNull(inv.getItem(6)).equals(item) ||
-                            !Objects.requireNonNull(inv.getItem(7)).equals(item) || !Objects.requireNonNull(inv.getItem(8)).equals(item)) {
-                        p.getInventory().remove(item);
-                    }
-
-                }
-            }
-
-        }
-    }
+//    private static void clearBarrier(Player p) {
+//
+//        for (ItemStack item : p.getInventory().getContents()) {
+//            if (item != null && item.getType() == Material.BARRIER) {
+//
+//                ItemMeta itemMeta = item.getItemMeta();
+//                NamespacedKey nbtKey = new NamespacedKey(ReinCarnationCore.inst(), "FixedBarrier");
+//                String nbtValue = itemMeta.getPersistentDataContainer().get(nbtKey, PersistentDataType.STRING);
+//                Inventory inv = p.getInventory();
+//
+//                if (nbtValue != null && nbtValue.equals("1")) {
+//
+//                    if (!Objects.requireNonNull(inv.getItem(5)).equals(item) || !Objects.requireNonNull(inv.getItem(6)).equals(item) ||
+//                            !Objects.requireNonNull(inv.getItem(7)).equals(item) || !Objects.requireNonNull(inv.getItem(8)).equals(item)) {
+//                        p.getInventory().remove(item);
+//                    }
+//
+//                }
+//            }
+//
+//        }
+//    }
 
     public static void Fixed() {
         Bukkit.getOnlinePlayers().forEach(FixedInventoryListener::barrier);
-        Bukkit.getOnlinePlayers().forEach(FixedInventoryListener::clearBarrier);
+//        Bukkit.getOnlinePlayers().forEach(FixedInventoryListener::clearBarrier);
     }
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
         int hotBar = e.getHotbarButton();
+        int slot = e.getSlot();
+        YamlConfiguration sabaminYml = ConfigFile.load(ReinCarnationCore.inst(), "sabamin.yml");
 
-        if ( hotBar >= 5 ) {
-            e.setCancelled(true);
-            Bukkit.broadcastMessage("うんちA");
+        if ( sabaminYml.getBoolean("players." + e.getWhoClicked().getUniqueId(), true) ) {
+            if (hotBar >= 0 && hotBar <= 9 || slot >= 5 && slot <= 8) {
+                e.setCancelled(true);
+            }
         }
-        Bukkit.broadcastMessage("うんちB");
+
     }
 
 }
